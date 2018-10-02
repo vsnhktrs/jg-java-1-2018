5b4d5db4555d9b47eb8e9124d094ec2681f462ea
Konstantins Buts <konstantins.buts@neotech.lv>
2018-10-02 21:11:46 +0300

    Add second lesson classes

--- src/lv/javaguru/lessons/l2/Task1.java
+++ src/lv/javaguru/lessons/l2/Task1.java
@@ -3,10 +3,19 @@ package lv.javaguru.lessons.l2;
 /**
  * @author Konstantins Buts on 01/10/2018.
  */
-public class Lesson2 {
+public class Task1 {
 
     public static void main(String[] args) {
+
         System.out.println("Welcome to the JavaGuru!");
+
+        int firstNumber = 5;
+        int secondNumber = 10;
+
+        int result = firstNumber + secondNumber;
+
+        System.out.println(result);
+
     }
 
 }
---
+++ src/lv/javaguru/lessons/l2/Task2.java
@@ -0,0 +1,43 @@
+package lv.javaguru.lessons.l2;
+
+/**
+ * @author Konstantins Buts on 02/10/2018.
+ */
+public class Task2 {
+
+    public static void main(String[] args) {
+
+        //Here we add numbers
+        long longNumber = 2;
+        int intNumber = 3;
+
+        long longResult = longNumber + intNumber;
+
+        System.out.println("Addition result = " + longResult);
+
+        //Here we subtract numbers
+        int firstSubtractNumber = 10;
+        float secondSubtractNumber = 3.14f;
+
+        float subtractionResult = firstSubtractNumber - secondSubtractNumber;
+
+        System.out.println("Subtraction result = " + subtractionResult);
+
+        //Here we multiply numbers
+        short firstMultiplier = 1000;
+        short secondMultiplier = 2000;
+
+        short multiplicationResult = (short) (firstMultiplier * secondMultiplier);
+
+        System.out.println("Multiplication result = " + multiplicationResult);
+
+        //Here we divide.. things
+        int firstValue = 20;
+        int secondValue = 5;
+        int divisionResult = firstValue / secondValue;
+
+        System.out.println("Division result = " + divisionResult);
+
+    }
+
+}
